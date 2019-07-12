eae7b4b74d6b82da7bfd69d0f8a8a725bebf6416
huanzi-qch <781792871@qq.com>
2019-07-12 13:17:12 +0800

    解决打成war后启动Tomcat报错的问题，详情请看博客：https://www.cnblogs.com/huanzi-qch/p/9948060.html

--- springboot-jar-war/src/main/java/cn/huanzi/qch/springbootjarwar/SpringbootJarWarApplication.java
+++ springboot-jar-war/src/main/java/cn/huanzi/qch/springbootjarwar/SpringbootJarWarApplication.java
@@ -2,22 +2,25 @@ package cn.huanzi.qch.springbootjarwar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //jar包
-@SpringBootApplication
-public class SpringbootJarWarApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootJarWarApplication.class, args);
-    }
-
-}
+//@SpringBootApplication
+//public class SpringbootJarWarApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringbootJarWarApplication.class, args);
+//    }
+//
+//}
 
 //war包
-/*@SpringBootApplication
-public class SpringbootJarWarApplication  extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SpringbootJarWarApplication  extends SpringBootServletInitializer implements WebApplicationInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringbootJarWarApplication.class);
@@ -26,9 +29,12 @@ public class SpringbootJarWarApplication  extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJarWarApplication.class, args);
     }
-}*/
+}
 
 
+/**
+ * 测试controller
+ */
 @RestController
 class IndexController{
 
