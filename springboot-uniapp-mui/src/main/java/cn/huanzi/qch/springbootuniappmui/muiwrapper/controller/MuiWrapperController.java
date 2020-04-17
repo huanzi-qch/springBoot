be12a65b6c9133e45deb256ef00927cda0fbefb9
huanzi-qch <781792871@qq.com>
2020-04-17 22:33:32 +0800

    1、新增按钮切换模式；

--- springboot-uniapp-mui/src/main/java/cn/huanzi/qch/springbootuniappmui/muiwrapper/controller/MuiWrapperController.java
+++ springboot-uniapp-mui/src/main/java/cn/huanzi/qch/springbootuniappmui/muiwrapper/controller/MuiWrapperController.java
@@ -1,5 +1,6 @@
 package cn.huanzi.qch.springbootuniappmui.muiwrapper.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,15 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/muiwrapper/")
 public class MuiWrapperController {
+    @Value("${huanzi.buttom.switch.mode}")
+    private String switchMode;
+
     //跳转主页面
     @GetMapping("main")
     public ModelAndView main() {
-        return new ModelAndView("muiwrapper/main");
+        ModelAndView modelAndView = new ModelAndView("muiwrapper/main");
+        modelAndView.addObject("switchMode",switchMode);
+        return modelAndView;
     }
 
     @GetMapping("dialog")
