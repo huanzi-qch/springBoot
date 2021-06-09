ca66fd824c2944b3fc37598ad62d3d36c0280f43
huanzi-qch <781792871@qq.com>
2021-06-09 15:46:51 +0800

    1、丝袜哥示例完善； 2、新增全局统一请求参数、全局统一响应；

--- springboot-swagger2/src/main/java/cn/huanzi/qch/springbootswagger2/config/SwaggerConfig.java
+++ springboot-swagger2/src/main/java/cn/huanzi/qch/springbootswagger2/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package cn.huanzi.qch.springbootswagger2.config;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.Parameter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Swagger UI 配置信息
@@ -21,6 +26,17 @@ public class SwaggerConfig {
      */
     @Bean
     public Docket controllerApi() {
+        //全局统一参数
+        List<Parameter> globalOperationParametersList = new ArrayList<>();
+        globalOperationParametersList.add(new ParameterBuilder().name("token").description("认证token").modelRef(new ModelRef("string")).parameterType("header").required(true).build());
+
+        //设置全局响应状态码
+        List<ResponseMessage> globalResponseMessageList = new ArrayList<>();
+        globalResponseMessageList.add(new ResponseMessageBuilder().code(401).message("没有认证，请重新登录").build());
+        globalResponseMessageList.add(new ResponseMessageBuilder().code(403).message("没有访问权限，请联系管理人员").build());
+        globalResponseMessageList.add(new ResponseMessageBuilder().code(404).message("找不到资源").build());
+        globalResponseMessageList.add(new ResponseMessageBuilder().code(500).message("服务器内部错误").build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
                         .title("API实时接口文档")
@@ -28,6 +44,9 @@ public class SwaggerConfig {
                         .contact(new Contact("huanzi-qch", "https://www.cnblogs.com/huanzi-qch/", ""))
                         .version("版本号:1.0")
                         .build())
+                .globalResponseMessage(RequestMethod.GET,globalResponseMessageList)
+                .globalResponseMessage(RequestMethod.POST,globalResponseMessageList)
+                .globalOperationParameters(globalOperationParametersList)
                 .select()
                 //API基础扫描路径
                 .apis(RequestHandlerSelectors.basePackage("cn.huanzi.qch.springbootswagger2.controller"))
--- springboot-swagger2/src/main/java/cn/huanzi/qch/springbootswagger2/controller/UserController.java
+++ springboot-swagger2/src/main/java/cn/huanzi/qch/springbootswagger2/controller/UserController.java
@@ -1,10 +1,10 @@
 package cn.huanzi.qch.springbootswagger2.controller;
 
 import cn.huanzi.qch.springbootswagger2.vo.UserVo;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("user")
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @ApiOperation(value = "根据id查询用户信息", notes = "查询数据库中某个的用户信息")
-    @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path", required = true)
+    @ApiImplicitParam(name = "id", value = "用户ID", paramType = "path",dataType="int", required = true)
     @GetMapping("get/{id}")
-    public String getUserById(@PathVariable int id) {
+    public String getUserById(@PathVariable Integer id) {
         if(id == 0){
             return "查无此人";
         }else{
@@ -22,14 +22,46 @@ public class UserController {
         }
     }
 
+    @ApiOperation(value = "根据id、name查询用户信息", notes = "查询数据库中某个的用户信息")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id", value = "用户ID", paramType = "query",dataType="int"),
+        @ApiImplicitParam(name = "name", value = "用户名称", paramType = "query",dataType="String"),
+        @ApiImplicitParam(name = "appId", value = "应用认证id", paramType = "header",dataType="String"),
+    })
+    @GetMapping("getUserByIdAndName")
+    public String getUserByIdAndName(HttpServletRequest request,Integer id, String name) {
+        if(id == 0){
+            return "查无此人";
+        }else{
+            return "{\"appId\":\""+request.getHeader("appId")+"\",\"id\":\""+id+"\",\"userName\":\""+name+"\"}";
+        }
+    }
+
     @ApiOperation(value = "根据UserVo对象查询用户信息", notes = "查询数据库中符合条件的用户信息")
-    @ApiImplicitParam(name = "userVo", value = "UserVo对象", paramType = "UserVo")
+    @ApiImplicitParam(paramType = "UserVo")
     @PostMapping("list")
-    public String getUserByUser(UserVo userVo) {
+    public String list(UserVo userVo) {
         if(userVo.getId() == 0){
             return "查无此人";
         }else{
             return userVo.toString();
+        }
+    }
+
+    @ApiOperation(value = "分页查询用户信息", notes = "查询数据库中符合条件的用户信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "当前页码", paramType = "query",dataType="int",defaultValue = "1"),
+            @ApiImplicitParam(name = "pageSize", value = "每页大小", paramType = "query",dataType="int",defaultValue = "10"),
+    })
+    @ApiResponses({
+            @ApiResponse(code=10001,message="xx业务规则不符合")
+    })
+    @PostMapping("page")
+    public String page(Integer page,Integer pageSize,UserVo userVo) {
+        if(userVo.getId() == 0){
+            return "查无此人";
+        }else{
+            return "page:"+page+",pageSize:"+pageSize+","+userVo.toString();
         }
     }
 
