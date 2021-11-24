6df1761cb26ff308bf13946513d66215d1319f75
huanzi-qch <781792871@qq.com>
2021-11-24 18:19:11 +0800

    1、解决RestControllerAdvice无法拦截Filter内抛出异常的bug；

--- springboot-exceptionhandler/src/main/java/cn/huanzi/qch/springbootexceptionhandler/config/ErrorPageConfig.java
+++ springboot-exceptionhandler/src/main/java/cn/huanzi/qch/springbootexceptionhandler/config/ErrorPageConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -22,7 +23,12 @@ public class ErrorPageConfig implements ErrorController{
     }
 
     @RequestMapping(ERROR_PATH)
-    public void errorPathHandler(HttpServletResponse response) {
+    public void errorPathHandler(HttpServletRequest request, HttpServletResponse response) throws Throwable{
+        //解决RestControllerAdvice无法拦截Filter内抛出异常
+        if (request.getAttribute("javax.servlet.error.exception") != null) {
+            throw (Throwable) request.getAttribute("javax.servlet.error.exception");
+        }
+
         //抛出ErrorPageException异常，方便被ExceptionHandlerConfig处理
         ErrorEnum errorEnum;
         switch (response.getStatus()) {
